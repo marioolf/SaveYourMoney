87ae37c5c5656c072d63b96b31ef67d81abcaad3
VirekLoch <39859315+VirekLoch@users.noreply.github.com>
2022-10-27 13:06:14 +0200

    Cabecera

--- view/layout/header.php
+++ view/layout/header.php
@@ -5,6 +5,7 @@
   <div class="menu">
     <img class="imagen" src="../../images/oie_transparent2.png">
     <button class="opciones"><a href="../gastos/gastos.php">Gastos</a></button>
+    <button class="opciones"><a href="../gastos/personal_area.php">Menu</a></button> 
     <button class="opciones"><a href="../users/login.php">Login</a></button>
       <?php if (isset($_SESSION["currentuser"])): ?>
         <div style="float:right">Hello <?=$_SESSION["currentuser"] ?> 
